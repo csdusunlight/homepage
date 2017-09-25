3fe1dd99341111084d70532b870ab95666d7f5cd
lvchunhui7 <lvchunhui7@126.com>
2017-09-25 22:09:52 +0800

    kwd

--- homepage/views.py
+++ homepage/views.py
@@ -4,7 +4,9 @@ from wafuli.models import SubscribeShip, Notice
 # Create your views here.
 
 def index(request):
-    recoms = SubscribeShip.objects.filter(user=request.user, is_recommend=True, is_on=True)[0:10]
+    recoms = list(SubscribeShip.objects.filter(user=request.user, is_recommend=True, is_on=True)[0:10])
+    if len(recoms)==0:
+        recoms = list(SubscribeShip.objects.filter(user=request.user, is_on=True)[0:4])
     recom_list = []
     for r in recoms:
         p = r.project
