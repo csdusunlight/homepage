dd0643680bf290960f0f980a74c2072351484d65
jiangzongyang <jiangzongyang197@163.com>
2017-09-28 11:51:53 +0800

    add m-homeapge

--- homepage/views.py
+++ homepage/views.py
@@ -25,3 +25,26 @@ def index(request):
         recom_list.append(data)
     notice_list = Notice.objects.filter(user=request.user)
     return render(request, 'my_homepage.html',{'recom_list':recom_list, 'notice_list':notice_list})
+
+    def m_homepage(request):
+        recoms = list(SubscribeShip.objects.filter(user=request.user, is_recommend=True, is_on=True)[0:10])
+        if len(recoms)==0:
+            recoms = list(SubscribeShip.objects.filter(user=request.user, is_on=True)[0:4])
+        recom_list = []
+        for r in recoms:
+            p = r.project
+            data = {
+                'id':p.id,
+                'title' : p.title,
+                'intrest': r.intrest if r.intrest else p.intrest,
+                'price': r.price if r.price else p.cprice,
+                'term': p.term,
+                'range': p.investrange,
+                'pic': p.picture_url(),
+                'investrange': p.investrange,
+                'strategy': p.strategy,
+                'state':p.state,
+            }
+            recom_list.append(data)
+        notice_list = Notice.objects.filter(user=request.user)
+        return render(request, 'm_homepage.html',{'recom_list':recom_list, 'notice_list':notice_list})
