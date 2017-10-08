a16ef3edaa5cd55f4bf3725f60657e56aebea6cb
server <you@example.com>
2017-10-08 11:26:33 +0800

    url

--- dragon/urls.py
+++ dragon/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
 #     url(r'^admin/', include(admin.site.urls)),
 #     url(r'^project/', include('project_admin.urls')),
 # #     url(r'^test/$', 'wafuli.views.index', name='captcha-refresh'),
-    url('^homepage/', include('homepage.urls')),  
+    url(r'^', include('homepage.urls')),  
 ]
 
 from django.conf.urls.static import static
