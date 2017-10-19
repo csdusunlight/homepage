7c6e4ec5b9fabb116514cca78ec1c2f540eb0d70
lvchunhui7 <lvchunhui7@126.com>
2017-10-19 19:32:37 +0800

    filter marks

--- dragon/middlewares.py
+++ dragon/middlewares.py
@@ -3,10 +3,14 @@ Created on 20170919
 
 @author: lch
 '''
+
+import logging
+logger=logging.getLogger('wafuli')
 from django.http.response import Http404
 from account.models import MyUser
 class SubdomainMiddleware(object):
     def process_request(self, request):
+        logger.info( request.META.keys())
         domain_parts = request.get_host().split('.')
         if len(domain_parts) == 3:
             qq_number = domain_parts[0]
