617cefc22375645eeb28e5cfab1f0cad53ae586d
jiangzongyang <jiangzongyang197@163.com>
2017-10-20 11:49:05 +0800

    none mark

--- dragon/middlewares.py
+++ dragon/middlewares.py
@@ -10,6 +10,7 @@ from django.http.response import Http404
 from account.models import MyUser
 class SubdomainMiddleware(object):
     def process_request(self, request):
+        logger.info( request.META.keys())
         domain_parts = request.get_host().split('.')
         if len(domain_parts) == 3:
             qq_number = domain_parts[0]
@@ -19,4 +20,5 @@ class SubdomainMiddleware(object):
                 request.user = MyUser.objects.get(qq_number=qq_number)
             except:
                 raise Http404
+        request.prehost= 'm'
         return None
