24ad350ff70348e8a49b1528d6c28850794cecf8
lch <690501772@qq.com>
2017-10-21 08:14:46 +0800

    url+moile

--- homepage/urls.py
+++ homepage/urls.py
@@ -12,7 +12,7 @@ import homepage.views
 # ]
 urlpatterns = [
     url(r'^$', homepage.views.index, name='hm_index'),
-    url(r'^mobile$', homepage.views.index),
+    url(r'^mobile/$', homepage.views.index),
     url(r'^detail/project/(?P<id>[0-9]+)/$', homepage.views.detail_project, name='detail_project'),
     url(r'^submitOrder/$', homepage.views.submitOrder, name='submitOrder'),
     url(r'^search/$', homepage.views.search, name='search'),
