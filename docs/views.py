5b442b245ae8a03df408f1c87e91f5d647a7c1af
lvchunhui7 <lvchunhui7@126.com>
2017-11-21 09:29:22 +0800

    view_count

--- docs/views.py
+++ docs/views.py
@@ -4,9 +4,12 @@ from docs.models import Document
 from django.contrib.auth.decorators import login_required
 from public.tools import login_required_ajax
 from django.http.response import JsonResponse, Http404
+from django.db.models import F
 
 # Create your views here.
 
 def display_doc(request, id):
     doc = Document.objects.get(id=id)
+    doc.view_count = F('view_count')+1
+    doc.save(update_fields=['view_count',])
     return render(request, 'display_doc.html', {'doc':doc})
